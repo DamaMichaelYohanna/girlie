c6918e978e0e1597691b23baf53d346fecdd75a4
= <get2dama11@gmail.com>
2024-01-08 18:54:19 +0100

    added basic authentication

--- account/views.py
+++ account/views.py
@@ -1,9 +1,23 @@
-from django.contrib.auth.views import auth_login, auth_logout
+from django.contrib import messages
+from django.contrib.auth import login as auth_login,logout as auth_logout, authenticate
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 
 def login(request):
+    if request.method == "POST":
+        username:str = request.POST.get("username")
+        password:str = request.POST.get("password")
+        print(username, password)
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                auth_login(request, user)
+                print("authenticated")
+                return redirect(reverse("main:index"))
+            else:
+                messages.error(request, "Authentication failed! ")
+
     return render(request, 'account/login.html')
 
 
