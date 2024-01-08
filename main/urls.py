b5b09e6a6ac15df4da4a748cfcc96fdc07443265
= <get2dama11@gmail.com>
2024-01-08 18:55:53 +0100

    added app name to url

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from main import views
 
-
+app_name = "main"
 urlpatterns = [
     path("", views.index, name="index"),
     path("about", views.about_us, name="about_us"),
