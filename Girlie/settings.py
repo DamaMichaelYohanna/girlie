1dde2eedd09e74dd60a949238fb8dce3bd4d3534
= <get2dama11@gmail.com>
2024-01-08 18:55:01 +0100

    remove settings for news_letter

--- Girlie/settings.py
+++ Girlie/settings.py
@@ -44,12 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'account',
     'main',
 
-
-    # 'newsletter',
 ]
 
 MIDDLEWARE = [
