d52230fcbf9d586f8da6f4cdac7b2842aec2f9a4
= <get2dama11@gmail.com>
2024-01-10 23:39:01 +0100

    Added new urls

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
 from user import views
-app_name = "user"
 
 urlpatterns = [
     path("login", views.login, name="login"),
     path('logout', views.logout, name='logout'),
+    path("register",views.register, name="register"),
+    path("verify/<uuid:otp>", views.verify_email, name="verify_email"),
     path("profile", views.profile, name="profile"),
-    path("register",views.register, name="register")
+    path("dashboard", views.dashboard, name="dashboard"),
+    # --------------------------------------------------
+    path("follow/<str:username>", views.follow_user, name="follow"),
+    path("unfollow/<str:username>", views.unfollow_user, name="unfollow"),
+    path("block/<str:username>", views.block_user, name="block"),
+    path("unblock/<str:username>", views.unblock_user, name="unblock"),
+
 ]
+app_name = "user"
